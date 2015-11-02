841f5150479219b201c248399e6d75a84b6ef9f3
n35n0m <n35n0m@gmail.com>
2015-11-02 14:49:46 +0100

    Updated gitignore to ignore .idea, main skeleton works with finnbot

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from zumo_button import ZumoButton          #Contains ZumoButton-class w/ wait_for_press method
 
+def main():
+    ZumoButton().wait_for_press()
+    print ("Main ended successfully!")
+
+main()
