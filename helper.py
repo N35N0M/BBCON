2bd0d957e173b64d3bb1e4b08b058833e9bd2645
n35n0m <n35n0m@gmail.com>
2015-11-02 11:55:57 +0100

    Beta code for helper files motob and sensob

--- helper.py
+++ helper.py
@@ -3,22 +3,55 @@ from camera import Camera
 from ultrasonic import Ultrasonic
 from reflectance_sensors import ReflectanceSensors
 
+## A Sensob can contain multiple sensor references, to be used in behaviors.
 class Sensob():
-    def __init__(self,sensor):
-        self.sensor = sensor
+    ## Constructor that initiates a senseob with all neccessary sensors
+    def __init__(self,sensors):
+        self.sensors = sensors
 
+    ## Makes each sensor associated with the senseob measure a new value
     def update(self):
         for sensor in self.sensors:
-            sensor.update()
+            sensor = sensor.update()
 
+    ## Gets the most recently measured value from each sensor
     def get_values(self):
+        values = []
         for sensor in self.sensors:
+            values.append(sensor.get_value())
 
+        return values
 
+    ## Resets each sensor, setting each value to None or -1, etc
     def reset(self):
+        for sensor in self.sensors:
+            sensor.reset()
 
 
+## Motor object. Contains several motor references and has the power to operationalize
+## all the motors it references
 class Motob():
 
-class Arbitrator():
+    #Constructor
+    def __init__(self, motors, value=0):
+        self.motors = motors    #Apparantly a list of motors
+        self.value = value      #The latest motor recommendation sent
 
+    #Updates the motor recommendation, and operationalizes it.
+    def update(self, motorValue):
+        self.value = motorValue
+        self.operationalize()
+
+    def operationalize(self):
+        ## Every motor can have a value between [-1,1]
+        ## Perhaps we should error-check to see if it's in range?
+        v = self.value
+
+        for motor in self.motors:
+            if v == 0:
+                motor.stop()
+            else:
+                motor.set_value(v)
+
+## Chooses
+class Arbitrator():
