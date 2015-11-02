a72ed48ba77d7d451456733f1f9ac23cc2329f23
n35n0m <n35n0m@gmail.com>
2015-11-02 15:41:51 +0100

    Testing main, added logging to main and BBCON

--- BBCON.py
+++ BBCON.py
@@ -1,63 +1,89 @@
 from helper import Arbitrator           #Arbitrator class
-from behavior import Behavior            #Behavior class
-import time
+from time import sleep
+import logging, sys
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+
+#DEBUG example: logging.debug('A debug message!')
+#INFO example: logging.info('We processed %d records', len(processed_records))
+#Other modes: WARNING, ERROR, CRITICAL
 
 ##Implents a Behavior Based Controller for Robotics Control
 class BBCON():
-
-
     def __init__(self, timesteplength):
-        self.behaviors = []
-        self.activeBehaviors = []
-        self.sensobs = []
-        self.motobs = []
-        self.timesteplength = timesteplength
-        self.arbitrator = Arbitrator()
+        self.behaviors = []                         #List of all behaviors
+        self.activeBehaviors = []                   #List of all behaviors currently monitored
+        self.senseobs = []                           #List of all unique sensobs, for updating
+        self.motobs = []                            #List of all unique motobs
+        self.timesteplength = timesteplength        #Time step (how often the robot should choose a new action)
+        self.arbitrator = Arbitrator(self,False)    #Arbitrator
+
+        logging.debug("Behavior-based Controller initialized")
 
     def get_activeBehaviors(self):
         return self.activeBehaviors
+
+        logging.debug("Active behaviors retrieved")
 
     #Appends a behavior object to a list
     def add_behavior(self, behavior):
         self.behaviors.append(behavior)
 
+        logging.debug("Behavior added to behavior list")
+
     #Appends a sense object to a list
     def add_senseob(self,senseob):
         self.senseobs.append(senseob)
+
+        logging.debug("Senseob added to senseob list")
 
     #Appends a behavior to a list
     def activate_behavior(self, behavior):
         if behavior in self.behaviors:
             self.activeBehaviors.append(behavior)
+            logging.debug("Behavior activated")
 
     #Removes a behavior from a list
     def deactivate_behavior(self, behavior):
         if behavior in self.active_behaviors:
             self.activeBehaviors.remove(behavior)
+            logging.debug("Behavior deactivated")
 
     #IN PROGRESS
     def run_one_timestep(self):
+        logging.debug("==== RUNNING TIMESTEP ====")
         self.update_senseobs()
         self.update_behaviors()
         self.arbitrator.choose_action()
         self.update_motobs()
-        time.sleep(self.timesteplength)
+        sleep(self.timesteplength)
         self.reset_sensobs()
+
+        logging.debug("==== TIMESTEP RUN SUCCESSFUL ====")
 
     #Updates the values for each sense object, called at each timestep
     def update_senseobs(self):
         for sense in self.senseobs:
             sense.update()
 
+        logging.debug("Senseobs updated")
+
     #Updates the values for each behavior object, called at each timestep
     def update_behaviors(self):
         for behavior in self.behaviors:
             behavior.update()
 
+        logging.debug("Behaviors updated")
+
     def update_motobs(self):
         for motor in self.motobs:
             motor.update()
 
+        logging.debug("Motobs updated")
+
     def reset_sensobs(self):
         for sense in self.senseobs:
             sense.reset()
+
+        logging.debug("Senseobs reset")
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from zumo_button import ZumoButton          #Contains ZumoButton-class w/ wait_for_press method
+from BBCON import BBCON
+
+import logging, sys
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 def main():
+    logging.debug("Main method initiated, waiting for button press")
     ZumoButton().wait_for_press()
-    print ("Main ended successfully!")
+
+    timeStep = 1
+
+    Controller = BBCON(timeStep)
+    Controller.run_one_timestep()
+
+    logging.debug("Main method terminated without error")
+
 
 main()
